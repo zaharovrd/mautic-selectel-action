ea326776b4a4605b61d02b23ef58a2f639c2b32e
roman <zaharovrd@gmail.com>
2025-12-29 15:20:41 +0300

    MAUTIC_TRUSTED_PROXIES=["0.0.0.0/0"]

--- scripts/mautic-deployer.ts
+++ scripts/mautic-deployer.ts
@@ -447,7 +447,7 @@ MAUTIC_DB_DATABASE=${this.config.mysqlDatabase}
 MAUTIC_DB_PORT=3306
 
 # Mautic Configuration
-MAUTIC_TRUSTED_PROXIES='["127.0.0.1","remote_addr","172.16.0.0/12","172.17.0.0/16"]'
+MAUTIC_TRUSTED_PROXIES=["0.0.0.0/0"]
 MAUTIC_RUN_CRON_JOBS=true
 MAUTIC_LOCALE=${this.config.mauticLocale || 'en_US'}
 MAUTIC_DEFAULT_TIMEZONE=${this.config.defaultTimezone || 'UTC'}
